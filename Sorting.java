c9716c689fbf99e38abe8463939cac48e9eed7bd
Akashmed <akashmed3@gmail.com>
2025-08-18 01:39:54 +0600

    bubble sort

---
+++ Sorting.java
@@ -0,0 +1,30 @@
+public class Sorting {
+    static void BubbleSort(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            boolean isSwap = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                    isSwap = true;
+                }
+            }
+            if(!isSwap){
+                break;
+            }
+        }
+        printArray(arr, n);
+    }
+
+    static void printArray(int[] arr, int n){
+        for(int i = 0 ; i< n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 4, 2, 1, 3, 5 };
+        BubbleSort(arr, 5);
+    }
+}
