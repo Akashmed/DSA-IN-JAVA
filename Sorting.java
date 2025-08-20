0ed3187082991f17e8fed02e673aadfc63f00f90
Akashmed <akashmed3@gmail.com>
2025-08-21 01:20:41 +0600

    selection sort

--- Sorting.java
+++ Sorting.java
@@ -4,27 +4,44 @@ public class Sorting {
             boolean isSwap = false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j+1];
-                    arr[j+1] = arr[j];
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
                     arr[j] = temp;
                     isSwap = true;
                 }
             }
-            if(!isSwap){
+            if (!isSwap) {
                 break;
             }
         }
         printArray(arr, n);
     }
 
-    static void printArray(int[] arr, int n){
-        for(int i = 0 ; i< n; i++){
+    static void SelectionSort(int[] arr, int n) {
+    
+        for (int i = 0; i < n-1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+
+        }
+        printArray(arr, n);
+    }
+
+    static void printArray(int[] arr, int n) {
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
     }
 
     public static void main(String[] args) {
         int[] arr = { 4, 2, 1, 3, 5 };
-        BubbleSort(arr, 5);
+        SelectionSort(arr, 5);
     }
 }
