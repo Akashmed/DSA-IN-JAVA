d001adf526ef80cda08047c17fc46fe00bff6864
Akashmed <akashmed3@gmail.com>
2025-08-21 02:20:33 +0600

    insertion sort

--- Sorting.java
+++ Sorting.java
@@ -18,8 +18,8 @@ public class Sorting {
     }
 
     static void SelectionSort(int[] arr, int n) {
-    
-        for (int i = 0; i < n-1; i++) {
+
+        for (int i = 0; i < n - 1; i++) {
             int minIdx = i;
             for (int j = i + 1; j < n; j++) {
                 if (arr[j] < arr[minIdx]) {
@@ -34,6 +34,21 @@ public class Sorting {
         printArray(arr, n);
     }
 
+    static void InsertionSort(int[] arr, int n) {
+        for (int i = 1; i < n; i++) {
+            int current = arr[i];
+            int prev = i - 1;
+
+            while (prev >= 0 && arr[prev] > current) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = current;
+        }
+
+        printArray(arr, n);
+    }
+
     static void printArray(int[] arr, int n) {
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
@@ -41,7 +56,7 @@ public class Sorting {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 4, 2, 1, 3, 5 };
-        SelectionSort(arr, 5);
+        int[] arr = { 4, 3, 2, 1, 5 };
+        InsertionSort(arr, 5);
     }
 }
