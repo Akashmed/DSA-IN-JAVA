7395cb31d725f17d9a878fa87a62f9d01ca03cd8
Akashmed <akashmed3@gmail.com>
2026-01-03 21:11:37 +0600

    implement queue using linked list

---
+++ Queue/LinkedListQueue.java
@@ -0,0 +1,55 @@
+package Queue;
+
+public class LinkedListQueue {
+    private class Node {
+        private int value;
+        private Node next;
+
+        private Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node front = null;
+    private Node rear = null;
+    private int size;
+
+    private boolean isEmpty() {
+        return front == null;
+    }
+
+    public void enqueue(int value) {
+        Node node = new Node(value);
+        if (isEmpty()) {
+            front = rear = node;
+        }
+        rear.next = node;
+        rear = node;
+        size++;
+    }
+
+    public void dequeue() {
+        if (isEmpty())
+            throw new IllegalStateException();
+        front = front.next;
+        size--;
+    }
+
+    public int peek() {
+        return front.value;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void print() {
+        Node current = front;
+        System.out.print("[ ");
+        while (current != null) {
+            System.out.print(current.value + " ");
+            current = current.next;
+        }
+        System.out.print("]");
+    }
+}
--- Queue/Main.java
+++ Queue/Main.java
@@ -5,16 +5,16 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        var q = new QueueReverser();
-        queue.add(10);
-        queue.add(20);
-        queue.add(40);
-        queue.add(50);
-        queue.add(60);
-//        q.reverse(queue);
-//        System.out.println(queue);
-        q.reverser(queue, 3);
+//        Queue<Integer> queue = new ArrayDeque<>();
+//        var q = new QueueReverser();
+//        queue.add(10);
+//        queue.add(20);
+//        queue.add(40);
+//        queue.add(50);
+//        queue.add(60);
+////        q.reverse(queue);
+////        System.out.println(queue);
+//        q.reverser(queue, 3);
 
 //        var queue = new ArrayQueue(5);
 //        queue.enqueue(5);
@@ -48,6 +48,15 @@ public class Main {
 //        while (!queue.isEmpty()){
 //            System.out.print(queue.remove() + " ");
 //        }
+
+        var queue = new LinkedListQueue();
+        queue.enqueue(5);
+        queue.enqueue(6);
+        queue.enqueue(7);
+        queue.dequeue();
+        queue.enqueue(9);
+        queue.print();
+        System.out.println(queue.peek());
 
     }
 
