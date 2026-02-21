c9912a10df237de27069c2172e0be814ec2623eb
Akashmed <akashmed3@gmail.com>
2026-02-21 21:51:18 +0600

    find first not repeating character

---
+++ Part_2/Hash_Tables/FirstNotRepeatedCharacter.java
@@ -0,0 +1,25 @@
+package Part_2.Hash_Tables;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstNotRepeatedCharacter {
+    private Map<Character, Integer> map = new HashMap<>();
+    public char find(String str){
+        var chars = str.toCharArray();
+
+        for(char ch : chars){
+            var count = map.containsKey(ch) ? map.get(ch) : 0;
+            map.put(ch, count + 1);
+        }
+
+        for(char ch : chars){
+            if(map.get(ch) == 1)
+                return ch;
+        }
+
+        return Character.MIN_VALUE;
+    }
+
+}
--- Part_2/Hash_Tables/Main.java
+++ Part_2/Hash_Tables/Main.java
@@ -6,9 +6,12 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, String> map = new HashMap<>();
-        map.put(1, "akash");
-        map.put(2, "ahmed");
-        System.out.println(map);
+//        Map<Integer, String> map = new HashMap<>();
+//        map.put(1, "akash");
+//        map.put(2, "ahmed");
+//        System.out.println(map.get(4));
+
+        var obj = new FirstNotRepeatedCharacter();
+        System.out.println(obj.find("a green apple"));
     }
 }
